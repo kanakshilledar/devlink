e6425e72cc4045ec064babc6d8f518b12b6f0275
Gaurav Patil <gauravpatil1628@gmail.com>
2024-06-14 14:48:28 +0530

    models: created structure for database
    Created EventInfo and User Structure.
    
    Signed-off-by: Gaurav Patil <gauravpatil1628@gmail.com>

---
+++ models/models.go
@@ -0,0 +1,24 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type User struct {
+	Id          primitive.ObjectID `json:"id,omitempty" bson:"_id"`
+	Name        string             `json:"name"`
+	PhoneNumber string             `json:"phone_number"`
+	Email       string             `json:"email"`
+	Password    string             `json:"password"`
+	Company     string             `json:"company"`
+}
+
+type EventInfo struct {
+	EventId     primitive.ObjectID `json:"event_id" bson:"event_id"`
+	EventName   string             `json:"event_name"`
+	StartDate   string             `json:"start_date"`
+	EndDate     string             `json:"end_date"`
+	Description string             `json:"description"`
+	EventType   string             `json:"event_type"`
+	Company     string             `json:"company"`
+}
