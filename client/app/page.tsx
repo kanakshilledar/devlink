1fd0af082d65547b6e4d8ecc5b6a805719df211e
Ajay <ajayramsaravanan4@gmail.com>
2024-09-22 10:44:34 +0530

    wip: Hero Section

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,5 +1,18 @@
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="w-full h-screen border-b-2 flex flex-col items-center justify-center">
+        <div className="text-5xl font-medium">DevLink</div>
+        <div className="w-5/6 mt-6 text-white/40">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, unde
+          nam illum, assumenda rerum voluptas eligendi eveniet ipsa nihil, nisi
+          quisquam dignissimos reprehenderit numquam nostrum? Ratione libero id
+          inventore ducimus!
+        </div>
+        <div className="">Find an Event...</div>
+      </div>
+    </div>
+  );
 };
 
 export default page;
