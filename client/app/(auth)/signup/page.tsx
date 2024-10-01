a2d19a4e04925daa904afd125afc253e1e626fce
Ajay <ajayramsaravanan4@gmail.com>
2024-10-01 15:53:26 +0530

    feat: Add API Integration for Sign Up

--- client/app/(auth)/signup/page.tsx
+++ client/app/(auth)/signup/page.tsx
@@ -1,38 +1,145 @@
+"use client";
+
+import { z } from "zod";
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import { SignUpSchema } from "@/lib/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useRouter } from "next/navigation";
 
 const page = () => {
-  const InputComponent = ({ title }: { title: string }) => {
-    return (
-      <div>
-        <div className="text-xl">{title}:</div>
-        <Input type="text" placeholder={title} className="mt-1" />
-      </div>
-    );
+  const router = useRouter();
+
+  const form = useForm<z.infer<typeof SignUpSchema>>({
+    resolver: zodResolver(SignUpSchema),
+    defaultValues: {
+      name: "",
+      phoneNumber: "",
+      email: "",
+      password: "",
+      company: "",
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof SignUpSchema>) => {
+    console.log(values);
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/createUser`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      const data = await res.json();
+      if (data) {
+        console.log(data);
+        setTimeout(() => {
+          router.push("/signin");
+        }, 2000);
+      }
+    } catch (error) {
+      console.error(error);
+    }
   };
+
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="flex flex-col gap-8 justify-between p-6 min-w-96 w-3/12 border-2">
-        <div className="text-4xl font-bold">SIGN UP</div>
-        <div>
-          <div className="flex flex-col gap-4">
-            <InputComponent title="Name" />
-            <InputComponent title="Phone Number" />
-            <InputComponent title="Email" />
-            <InputComponent title="Password" />
-            <InputComponent title="Company" />
-          </div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="flex flex-col gap-4 justify-between p-6 w-11/12 md:w-3/12 border-2"
+        >
+          <div className="text-4xl font-bold">SIGN UP</div>
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-xl">Name:</FormLabel>
+                <FormControl>
+                  <Input placeholder="Name" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="phoneNumber"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-xl">Phone Number:</FormLabel>
+                <FormControl>
+                  <Input placeholder="Phone Number" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-xl">Email:</FormLabel>
+                <FormControl>
+                  <Input placeholder="Email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-xl">Password:</FormLabel>
+                <FormControl>
+                  <Input placeholder="Password" type="password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="company"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel className="text-xl">Company:</FormLabel>
+                <FormControl>
+                  <Input placeholder="Company" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
           <div className="mt-2">
             Have an account? Login in{" "}
             <Link href="/signin">
               <u>here</u>
             </Link>
           </div>
-        </div>
-        <div className="px-4 py-2 border-2 hover:bg-white hover:text-black cursor-pointer text-center">
-          Sign In
-        </div>
-      </div>
+          <Button
+            className="px-4 py-6 border-2 bg-transparent hover:bg-white hover:text-black cursor-pointer text-center rounded-none text-lg"
+            type="submit"
+          >
+            Sign Up
+          </Button>
+        </form>
+      </Form>
     </div>
   );
 };
--- client/lib/types.ts
+++ client/lib/types.ts
@@ -1,3 +1,5 @@
+import { z } from "zod";
+
 export interface EventCardType {
   eventId: string;
   eventName: string;
@@ -8,3 +10,16 @@ export interface EventCardType {
   company: string;
   addedBy: string;
 }
+
+export const SignInSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export const SignUpSchema = z.object({
+  name: z.string().min(3),
+  phoneNumber: z.string().min(10),
+  email: z.string().email(),
+  password: z.string().min(6),
+  company: z.string().min(3),
+});
