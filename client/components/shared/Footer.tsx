612f09c69d52036f5d5376d01cb96d2f99d856ed
Ajay <ajayramsaravanan4@gmail.com>
2024-10-01 12:27:55 +0530

    refactor: Home page

---
+++ client/components/shared/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div className="text-center my-6">&lt;footer&gt;</div>;
+};
+
+export default Footer;
---
+++ client/lib/types.ts
@@ -0,0 +1,10 @@
+export interface EventCardType {
+  eventId: string;
+  eventName: string;
+  startDate: string;
+  endDate: string;
+  description: string;
+  eventType: string;
+  company: string;
+  addedBy: string;
+}
