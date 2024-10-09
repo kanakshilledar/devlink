63961cb9f151463b844fffc22d6aa61d41b2f56e
Ajay <ajayramsaravanan4@gmail.com>
2024-10-09 23:14:03 +0530

    fix: Footer Responsiveness

--- client/components/shared/Footer.tsx
+++ client/components/shared/Footer.tsx
@@ -6,10 +6,10 @@ const Footer = () => {
   const router = useRouter();
   return (
     <div className="mt-6 border-t flex flex-col items-center justify-center w-full">
-      <div className="w-4/5 flex justify-between py-16">
+      <div className="w-4/5 flex flex-col gap-6 md:gap-0 md:flex-row justify-between py-16">
         <div className="basis-1/2">
           <div className="text-4xl font-bold">DevLink</div>
-          <div className="text-neutral-400 w-4/5 text-xl mt-2">
+          <div className="text-neutral-400 w-full md:w-4/5 text-xl mt-2">
             Discover tech conferences, hackathons, and meetups near you.
             Contribute and connect with the tech community. Join us in making
             event discovery simple!
@@ -17,7 +17,7 @@ const Footer = () => {
         </div>
         <div className="flex flex-col gap-2">
           <div
-            className="self-end hover:underline cursor-pointer text-xl"
+            className="hover:underline cursor-pointer text-xl"
             onClick={() => {
               router.push("/about");
             }}
@@ -25,7 +25,7 @@ const Footer = () => {
             About Us
           </div>
           <div
-            className="self-end hover:underline cursor-pointer text-xl"
+            className="hover:underline cursor-pointer text-xl"
             onClick={() => {
               router.push("/termsofuse");
             }}
