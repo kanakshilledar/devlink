a797b8ed8a72ea2a6395dd1c11eb7049a9f4a265
Ajay <ajayramsaravanan4@gmail.com>
2024-10-09 12:56:35 +0530

    feat: Add About Page and modify Footer

--- client/components/shared/Footer.tsx
+++ client/components/shared/Footer.tsx
@@ -1,5 +1,41 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 const Footer = () => {
-  return <div className="text-center my-6">&lt;footer&gt;</div>;
+  const router = useRouter();
+  return (
+    <div className="mt-6 border-t flex flex-col items-center justify-center w-full">
+      <div className="w-4/5 flex justify-between py-16">
+        <div className="basis-1/2">
+          <div className="text-4xl font-bold">DevLink</div>
+          <div className="text-neutral-400 w-4/5 text-xl mt-2">
+            Discover tech conferences, hackathons, and meetups near you.
+            Contribute and connect with the tech community. Join us in making
+            event discovery simple!
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <div
+            className="self-end hover:underline cursor-pointer text-xl"
+            onClick={() => {
+              router.push("/about");
+            }}
+          >
+            About Us
+          </div>
+          <div className="self-end hover:underline cursor-pointer text-xl">
+            Terms of Use
+          </div>
+        </div>
+      </div>
+      <div className="border-t py-4 w-full text-center">
+        <div className="text-neutral-300 text-sm">
+          &copy; 2024 DevLink. All rights reserved.
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
