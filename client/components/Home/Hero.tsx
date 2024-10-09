c145d09c3db60d9098c89670a1d0c81b5e3ec0b9
Ajay <ajayramsaravanan4@gmail.com>
2024-10-09 23:04:56 +0530

    feat: Add Hero Animation

--- client/components/Home/Hero.tsx
+++ client/components/Home/Hero.tsx
@@ -1,35 +1,52 @@
 "use client";
-import { Input } from "@/components/ui/input";
 import Link from "next/link";
+import TypeIt from "typeit-react";
 
 const Hero = () => {
-  const token = typeof window !== 'undefined' ? localStorage.getItem('token') : null
+  const token = localStorage.getItem("token");
+
   return (
     <div className="w-full h-screen border-b-2 flex flex-col">
-      {token ? (
-        <div></div>
-      ) : (
+      {!token && (
         <div className="flex justify-end px-12 py-6">
           <Link
             href="/signin"
-            className="px-4 py-2 border-2 hover:bg-white hover:text-black cursor-pointer"
+            className="px-4 py-2 border-2 hover:bg-white hover:text-black cursor-pointer rounded-lg"
           >
             Sign In
           </Link>
         </div>
       )}
-      <div className="flex flex-col justify-center gap-6 items-center grow">
-        <div className="text-5xl font-bold">DevLink</div>
-        <div className="w-5/6 text-white/40 text-2xl">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, unde
-          nam illum, assumenda rerum voluptas eligendi eveniet ipsa nihil, nisi
-          quisquam dignissimos reprehenderit numquam nostrum? Ratione libero id
-          inventore ducimus!
+      <div className="flex flex-col justify-center gap-6 grow px-16">
+        <div className="text-7xl font-bold">DevLink</div>
+        <div className="text-4xl font-semibold">
+          Let's find you a{" "}
+          <TypeIt
+            options={{
+              loop: true,
+              speed: 150,
+              deleteSpeed: 75,
+              nextStringDelay: 1000,
+              waitUntilVisible: true,
+            }}
+            getBeforeInit={(instance) => {
+              instance
+                .type("Meetup")
+                .pause(1000)
+                .delete(6)
+                .type("Conference")
+                .pause(1000)
+                .delete(10)
+                .type("Summit")
+                .pause(1000)
+                .delete(6)
+                .type("Hackathon")
+                .pause(1000)
+                .delete(9);
+              return instance;
+            }}
+          />
         </div>
-        <Input
-          className="bg-[#121212] w-2/5 px-4 py-6 border-2 border-white/20 rounded-lg text-xl"
-          placeholder="Find an Event"
-        />
       </div>
     </div>
   );
