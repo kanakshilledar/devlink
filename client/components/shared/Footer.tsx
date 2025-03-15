4c3ed8ac4883863c70d254a451fb8bbc9d6d232c
aps1129 <arnavsingh4121@gmail.com>
2025-03-15 18:40:15 +0530

    Update Footer.tsx

--- client/components/shared/Footer.tsx
+++ client/components/shared/Footer.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
 const Footer = () => {
-  const router = useRouter();
+  // const router = useRouter();
   return (
     <div className="mt-6 border-t flex flex-col items-center justify-center w-full">
       <div className="w-4/5 flex flex-col gap-6 md:gap-0 md:flex-row justify-between py-16">
